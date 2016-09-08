36a13722aa360c6f8b56ca477298fda9a1300046
ckuh <kuh_chris@yahoo.com>
2016-09-08 15:10:46 -0700

    arrow functions

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,12 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const MyTitle = require('./MyTitle')
 
-const MyFirstComponent = function () {
-  return (
-    <div>
-      <MyTitle title='Whatevs' color='rebeccapurple' />
-      <MyTitle title='Hello World!' color='dodgerblue' />
-      <MyTitle title='React' color='#f06d06' />
-    </div>
-  )
-}
+const MyFirstComponent = () => (
+  <div>
+    <MyTitle title='Whatevs' color='rebeccapurple' />
+    <MyTitle title='Hello World!' color='dodgerblue' />
+    <MyTitle title='React' color='#f06d06' />
+  </div>
+)
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
