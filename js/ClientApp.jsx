e3ac98d2894b45fbc7a9bb12bb0812c71130f775
ckuh <kuh_chris@yahoo.com>
2016-09-09 02:15:05 -0700

    index route

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,15 +1,18 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-const { Router, Route, hashHistory } = require('react-router')
+const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 
 // components
 const Landing = require('./Landing')
 const Search = require('./Search')
+const Layout = require('./Layout')
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 
