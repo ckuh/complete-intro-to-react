716b808dae24117b63d2156b6df895396fc0b647
ckuh <kuh_chris@yahoo.com>
2016-09-12 14:37:10 -0700

    link to details

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,8 +1,11 @@
 const React = require('react')
+import { Link } from 'react-router'
 
 const ShowCard = (props) => (
   <div className='show-card'>
-    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+    <Link to={`/details/${props.imdbID}`}>
+      <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+    </Link>
     <div className='show-card-text'>
       <h3 className='show-card-title'>{props.title}</h3>
       <h4 className='show-card-year'>({props.year})</h4>
@@ -17,7 +20,8 @@ ShowCard.propTypes = {
   poster: string.isRequired,
   title: string.isRequired,
   year: string.isRequired,
-  description: string.isRequired
+  description: string.isRequired,
+  imdbID: string.isRequired
 }
 
 module.exports = ShowCard
